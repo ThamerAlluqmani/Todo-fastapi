b873b18dd9a39cc90709c7f9c2bacbc5e79a3585
Thamer Alluqmani <thamer.allogmani@gmail.com>
2023-06-18 12:05:16 +0300

    project structure

---
+++ app/database.py
@@ -0,0 +1,8 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('postgresql://root:root@localhost:5432/CRUD_FAST_API', echo=True)
+Base = declarative_base()
+
+SessionLocal = sessionmaker(bind=engine)
