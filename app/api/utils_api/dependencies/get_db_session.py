2da9518a147a5a8c4f4274bdcdd0d0f820f9bc6d
Thamer Alluqmani <thamer.allogmani@gmail.com>
2023-06-20 11:28:25 +0300

    Update get_db_session.py

--- app/api/utils_api/dependencies/get_db_session.py
+++ app/api/utils_api/dependencies/get_db_session.py
@@ -1,14 +1,9 @@
 from sqlalchemy.orm.session import Session
-
 from app.database import Session
 
 
 def get_db_session():
-    session: Session = Session()
-    try:
-        yield session
-    except:
-        session.rollback()
-        raise
-    finally:
-        session.close()
+    with Session() as s:
+        with s.begin():
+            yield s
+            s.commit()
